6d62aed92ab23b5213f775d8af78f393bff560e6
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2013-03-21 01:19:44 -0600

    Initial Commit

---
+++ PBX Forwarder/PBX_Forwarder.h
@@ -0,0 +1,15 @@
+//
+//  PBX_Forwarder.h
+//  PBX Forwarder
+//
+//  Created by Ernesto MB on 21/03/13.
+//  Copyright (c) 2013 Ernesto MB. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface PBX_Forwarder : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ PBX Forwarder/PBX_Forwarder.m
@@ -0,0 +1,17 @@
+//
+//  PBX_Forwarder.m
+//  PBX Forwarder
+//
+//  Created by Ernesto MB on 21/03/13.
+//  Copyright (c) 2013 Ernesto MB. All rights reserved.
+//
+
+#import "PBX_Forwarder.h"
+
+@implementation PBX_Forwarder
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
