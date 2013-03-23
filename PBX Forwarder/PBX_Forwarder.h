d592ef0b28bf950d6cb8187b323892945fa2c6a4
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2013-03-23 01:13:46 -0600

    GUI básico y persistencia de datos.

--- PBX Forwarder/PBX_Forwarder.h
+++ PBX Forwarder/PBX_Forwarder.h
@@ -8,15 +8,34 @@
 
 #import <PreferencePanes/PreferencePanes.h>
 
+#define KEY_TOGGLE_FORWARDING CFSTR("Toggle forwarding")
+#define KEY_EXTENSION_NUMBER CFSTR("Extension number")
+#define KEY_EXTENSION_PASSWORD CFSTR("Extension password")
+#define KEY_TARGET_FORWARDING_NUMBER CFSTR("Target forwarding number")
+
+
 @interface PBX_Forwarder : NSPreferencePane
 {
-    IBOutlet NSProgressIndicator *progressIndicator;
-    IBOutlet NSNumberFormatter *extensionNumber;
+    IBOutlet NSButton *forwardingToggler;
+    IBOutlet NSTextField *extensionNumber;
     IBOutlet NSSecureTextField *extensionPassword;
-    IBOutlet NSNumberFormatter *targetForwardingNumber;
-    IBOutlet NSButton *toggleForwarding;
-    
+    IBOutlet NSTextField *targetForwardingNumber;
+    IBOutlet NSProgressIndicator *progressIndicator;
+    IBOutlet NSTextField *errorMessage;
+    CFStringRef appID;
 }
+
+- (IBAction)preferenceDidChange:(id)sender;
+
 - (void)mainViewDidLoad;
+
+- (id)getPreferenceValueForKey:(CFStringRef)key withType:(CFTypeID)type;
+
+- (bool)getBooleanPreferenceValueForKey:(CFStringRef)key;
+
+- (id)getStringPreferenceValueForKey:(CFStringRef)key;
+
+- (void)setPreferenceValueForKey:(CFStringRef)key withValue:(CFPropertyListRef)value;
+
 
 @end
--- PBX Forwarder/PBX_Forwarder.m
+++ PBX Forwarder/PBX_Forwarder.m
@@ -11,9 +11,98 @@
 @implementation PBX_Forwarder
 
 
+- (id)initWithBundle:(NSBundle *)bundle
+{
+    if ((self = [super initWithBundle:bundle]) != nil ) {
+        appID = CFSTR("mx.menta.pbx-forwarder");
+    }
+    
+    return self;
+}
+
+- (id)getPreferenceValueForKey:(CFStringRef)key withType:(CFTypeID)type
+{
+    CFPropertyListRef value = CFPreferencesCopyAppValue(key, appID);
+    
+    if (type == CFBooleanGetTypeID()) {
+        if (value && CFGetTypeID(value) == type) {
+            return (id)CFBooleanGetValue(value);
+        }
+        return NO;
+    } else if (type == CFStringGetTypeID()) {
+        if (value && CFGetTypeID(value) == type) {
+            return (NSString *)value;
+        }
+    }
+    if (value) {
+        CFRelease(value);
+    }
+    return @"";
+}
+
+- (bool)getBooleanPreferenceValueForKey:(CFStringRef)key
+{
+    return [self getPreferenceValueForKey:key withType:CFBooleanGetTypeID()];
+}
+
+- (id)getStringPreferenceValueForKey:(CFStringRef)key
+{
+    return [self getPreferenceValueForKey:key withType:CFStringGetTypeID()];
+}
+
+- (void)setPreferenceValueForKey:(CFStringRef)key withValue:(CFPropertyListRef)value
+{
+    CFNotificationCenterRef center;
+    CFPreferencesSetAppValue(key, value, appID);
+    CFPreferencesAppSynchronize(appID);
+    center = CFNotificationCenterGetDistributedCenter();
+    CFNotificationCenterPostNotification(center, CFSTR("Menta PBX preferences changed"), NULL, NULL, TRUE);
+}
+
 - (void)mainViewDidLoad
 {
-    NSLog(@"OK OK OK");
+    NSLog(@"mainViewDidLoad");
+    
+    [forwardingToggler setState:[self getBooleanPreferenceValueForKey:KEY_TOGGLE_FORWARDING]];
+    [extensionNumber setStringValue:[self getStringPreferenceValueForKey:KEY_EXTENSION_NUMBER]];
+    [extensionPassword setStringValue:[self getStringPreferenceValueForKey:KEY_EXTENSION_PASSWORD]];
+    [targetForwardingNumber setStringValue:[self getStringPreferenceValueForKey:KEY_TARGET_FORWARDING_NUMBER]];
+    
+    [self updateForwardingTogglerButton];
+    
+    NSLog(@"state %@", [forwardingToggler stringValue]);
+    
 }
+
+- (void)updateForwardingTogglerButton
+{
+    [forwardingToggler setTitle:[forwardingToggler state] ? @"DESACTIVAR FORWARDING" : @"ACTIVAR FORWARDING"];
+}
+
+- (IBAction)preferenceDidChange:(id)sender
+{
+
+    [progressIndicator setHidden:FALSE];
+    [progressIndicator startAnimation:self];
+    
+    if (sender == forwardingToggler) {
+        [self setPreferenceValueForKey:KEY_TOGGLE_FORWARDING withValue:[forwardingToggler state] ? kCFBooleanTrue : kCFBooleanFalse];
+        [self updateForwardingTogglerButton];
+        
+    } else if (sender == extensionNumber) {
+        [self setPreferenceValueForKey:KEY_EXTENSION_NUMBER withValue:[extensionNumber stringValue]];
+    } else if (sender == extensionPassword) {
+        [self setPreferenceValueForKey:KEY_EXTENSION_PASSWORD withValue:[extensionPassword stringValue]];
+    } else if (sender == targetForwardingNumber) {
+        [self setPreferenceValueForKey:KEY_TARGET_FORWARDING_NUMBER withValue:[targetForwardingNumber stringValue]];
+    }
+    
+    [progressIndicator stopAnimation:self];
+    [progressIndicator setHidden:TRUE];
+    
+    NSLog(@"Saving %@ = %@ in preferenceDidChange", sender, [sender stringValue]);
+}
+
+
 
 @end
