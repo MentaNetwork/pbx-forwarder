5ddd0a373a0a54740f716147f1e0fb599a1813e0
Ernesto Mendoza Blanco <ernestom@menta.com.mx>
2013-03-24 23:44:36 -0600

    Script para hacer una app a partir de un .py.

---
+++ appbuilder.py
@@ -0,0 +1,83 @@
+from sys import argv
+from os import mkdir, path, chmod, stat
+from stat import S_IXUSR, S_IXGRP, S_IXOTH
+
+if len(argv) != 3:
+    print 'Usage: $ python appbuilder.py sourceapp.py TargetApp.app'
+    exit()
+
+script_name = argv[1]
+app_name = argv[2]
+contents_path = path.join(app_name, 'Contents')
+macos_path = path.join(contents_path, 'MacOS')
+version = '1.0.0'
+bundle_name = app_name.split('.')[0]
+bundle_identifier = app_name
+
+info_plist_tpl = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleDevelopmentRegion</key>
+    <string>English</string>
+    <key>CFBundleExecutable</key>
+    <string>main.py</string>
+    <key>CFBundleGetInfoString</key>
+    <string>%s</string>
+    <key>CFBundleIconFile</key>
+    <string>app.icns</string>
+    <key>CFBundleIdentifier</key>
+    <string>%s</string>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
+    <key>CFBundleName</key>
+    <string>%s</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleShortVersionString</key>
+    <string>%s</string>
+    <key>CFBundleSignature</key>
+    <string>????</string>
+    <key>CFBundleVersion</key>
+    <string>%s</string>
+    <key>NSAppleScriptEnabled</key>
+    <string>YES</string>
+    <key>NSMainNibFile</key>
+    <string>MainMenu</string>
+    <key>NSPrincipalClass</key>
+    <string>NSApplication</string>
+</dict>
+</plist>"""
+
+
+def make_dir(dir_name):
+    try:
+        mkdir(dir_name)
+    except Exception, e:
+        print e
+
+def write_file(base_path, file, contents):
+    f = open(path.join(base_path, file), 'w')
+    f.write(contents)
+    f.close()
+
+def main():
+    make_dir(app_name)
+    make_dir(contents_path)
+    make_dir(macos_path)
+    info_plist_vars = (bundle_name + ' ' + version,
+                       bundle_identifier,
+                       bundle_name,
+                       bundle_name + ' ' + version, version)
+    info_plist_contents = info_plist_tpl % info_plist_vars
+    write_file(contents_path, 'Info.plist', info_plist_contents)
+    write_file(contents_path, 'PkgInfo', 'APPL????')
+    write_file(macos_path, script_name, open(script_name).read())
+    old_mode = stat(path.join(macos_path, script_name)).st_mode
+    new_mode = old_mode | S_IXUSR | S_IXGRP | S_IXOTH
+    chmod(path.join(macos_path, script_name), new_mode)
+    print 'App built in %s' % app_name
+
+if __name__ == '__main__':
+    main()
+    
